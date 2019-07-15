dacfed6f92339d6df861d04bcb9bf22433a251e1
Brayden Tan <algohex@gmail.com>
2019-07-15 23:05:58 +0800

    initial commit

---
+++ includes/class-wp-post-analytics-by-algohex-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://algohex.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Post_Analytics_By_Algohex
+ * @subpackage Wp_Post_Analytics_By_Algohex/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Post_Analytics_By_Algohex
+ * @subpackage Wp_Post_Analytics_By_Algohex/includes
+ * @author     Algohex Web Developer Team <algohex@gmail.com>
+ */
+class Wp_Post_Analytics_By_Algohex_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-post-analytics-by-algohex-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://algohex.com
+ * @since      1.0.0
+ *
+ * @package    Wp_Post_Analytics_By_Algohex
+ * @subpackage Wp_Post_Analytics_By_Algohex/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Post_Analytics_By_Algohex
+ * @subpackage Wp_Post_Analytics_By_Algohex/includes
+ * @author     Algohex Web Developer Team <algohex@gmail.com>
+ */
+class Wp_Post_Analytics_By_Algohex_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
