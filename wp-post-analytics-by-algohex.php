49b0cee21f63a1fde417149d17c2dbe94d13f56e
Brayden Tan <algohex@gmail.com>
2019-07-29 22:37:33 +0800

    update license

--- wp-post-analytics-by-algohex.php
+++ wp-post-analytics-by-algohex.php
@@ -18,7 +18,7 @@
  * Version:           0.1.0
  * Author:            Algohex Web Developer Team
  * Author URI:        https://algohex.com
- * License:           GPL-2.0+
+ * License:           GPL-3.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wp-post-analytics-by-algohex
  * Domain Path:       /languages
